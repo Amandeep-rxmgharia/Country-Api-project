8b074656fd9917136558ed55a0fa11e62c7eb666
Amandeep-rxmgharia <amn.rxmgharia@gmail.com>
2025-09-17 09:48:08 +0530

    correct filter by region

--- components/SelectMenu.jsx
+++ components/SelectMenu.jsx
@@ -5,7 +5,7 @@ setQuery(e.target.value.toLowerCase())
   }
   return (
     <select onChange={changeHandler} className="filter-by-region">
-      <option hidden="">Filter by Region</option>
+      <option value="" hidden="">Filter by Region</option>
       <option value="Africa">Africa</option>
       <option value="America">America</option>
       <option value="Asia">Asia</option>
